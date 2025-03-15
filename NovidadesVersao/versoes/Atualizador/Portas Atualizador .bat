61a5cdcb8a48fd416ece2dfeb20ecfc06591295a
Anderson De Souza <65574525+souzaseven@users.noreply.github.com>
2025-03-15 09:21:19 -0400

    Delete NovidadesVersao/versoes directory

--- NovidadesVersao/versoes/Atualizador/Portas Atualizador .bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Configurando regras de firewall para o Atualizador TGA Funcionar...
-
-netsh advfirewall firewall add rule name="Atualizador TGA Entrada" dir=in protocol=TCP localport=20002 action=allow
-netsh advfirewall firewall add rule name="Atualizador TGA Saída" dir=out protocol=TCP localport=20002 action=allow
-
-echo Regras de firewall configuradas com sucesso!
-exit
