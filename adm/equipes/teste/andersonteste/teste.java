f4d4c15dd848db0c9d737ea1a83df4791f55c09e
Anderson De Souza <65574525+souzaseven@users.noreply.github.com>
2026-05-28 20:14:52 -0400

    Create teste.java

---
+++ adm/equipes/teste/andersonteste/teste.java
@@ -0,0 +1,12 @@
+public class BoasVindas {
+
+    public static void main(String[] args) {
+        System.out.println("============================================");
+        System.out.println("         Bem-vindo ao Sistema TGA!         ");
+        System.out.println("============================================");
+        System.out.println("  Olá! Seja muito bem-vindo(a).            ");
+        System.out.println("  Esperamos que tenha um ótimo dia!        ");
+        System.out.println("============================================");
+    }
+
+}
